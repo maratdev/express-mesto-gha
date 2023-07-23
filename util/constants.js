78a9695d4a9f80fe4d002f4885cd7cfdb61ab10d
maratdev <voredev@gmail.com>
2023-07-24 01:53:59 +0300

    feat(ПР15) app update

--- util/constants.js
+++ util/constants.js
@@ -2,7 +2,7 @@ const COOKIE_MAX_AGE = 3600000; // 7d
 const JWT_TOKEN_EXPIRES = '7d';
 // limiter
 const TIME_LIMIT = 15 * 60 * 1000; // за 15 минут
-const MAX_LIMIT = 200; // можно совершить максимум 100 запросов с одного IP
+const MAX_LIMIT = 400; // можно совершить максимум 100 запросов с одного IP
 
 // RegExp
 const IS_URL = /^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s!"#'()*+,:;<>@[\\\]`{|}~]*$/;
