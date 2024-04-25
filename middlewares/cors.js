4bd0d4c1f65bcbeeed5bd9e89cf8a16dcaa1e174
maratdev <starspersona@gmail.com>
2024-04-26 01:16:37 +0300

    feat: update controller/auth.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,9 +1,6 @@
-const { NODE_ENV, ALLOWED_CORS_PRODUCTION } = process.env;
-const ALLOWED_CORS = NODE_ENV === 'production' ? ALLOWED_CORS_PRODUCTION.split(', ') : ['https://localhost:3000', 'http://localhost:3000', 'localhost:3000'];
-
 const CORS_OPTIONS = {
   credentials: true,
-  origin: ALLOWED_CORS,
+  origin: 'http://localhost:3000',
   exposedHeaders: ['set-cookie'],
 };
 
