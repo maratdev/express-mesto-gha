07d3fbc71ee3d5362afbfbb50b2b9c648bd23524
maratdev <starspersona@gmail.com>
2024-04-26 22:27:31 +0300

    feat: update controller/auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
-
+  console.log(token);
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'prpZUoYKk3YJ3nhemFHZ');
