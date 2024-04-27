c13fc963f60799d9dd3bad47f14afdb19c2da113
maratdev <starspersona@gmail.com>
2024-04-27 20:45:57 +0300

    feat: update controller/auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,6 +45,7 @@ const login = (req, res, next) => {
         maxAge: COOKIE_MAX_AGE,
         httpOnly: true,
         secure: false,
+        signed: true,
       });
       res.send({ token });
     })
