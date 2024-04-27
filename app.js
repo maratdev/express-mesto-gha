4e55170fab0eb100a98948b46763d7442aad2068
maratdev <starspersona@gmail.com>
2024-04-27 20:52:27 +0300

    feat: update controller/auth.js

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ const limiter = rateLimit({
 app.use(limiter);
 app.use(cors({
   origin: ['http://api.voredev.ru', 'http://localhost:3000'],
-  credentials: true,
 }));
 app.use(express.json());
 app.use(cookieParser(JWT_SECRET));
